0d8806a4322ee8f0a3b9323c1e90bc1b8a67bfcc
Alex <acfujimoto@gmail.com>
2015-11-21 20:43:42 -0500

    add a logout setting     --known error incorect password closes app(non crash) instead of staying of login activity

--- app/src/main/java/com/meltysnowowl/bestflush/MainActivity.java
+++ app/src/main/java/com/meltysnowowl/bestflush/MainActivity.java
@@ -1,14 +1,20 @@
 package com.meltysnowowl.bestflush;
 
+import android.app.AlertDialog;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity implements MapFragment.OnFragmentInteractionListener{
 
@@ -34,6 +40,38 @@ public class MainActivity extends AppCompatActivity implements MapFragment.OnFra
 
     @Override
     public void onFragmentInteraction(Uri uri) {
-        
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_logout:
+                ParseUser.logOut();
+                navigateToLogin();
+                break;
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void navigateToLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
